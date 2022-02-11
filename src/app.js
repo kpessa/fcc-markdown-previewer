2a64c9d75634e847d6fdafab17cda85c1f10c3e8
Kurt Pessa <kpessa@gmail.com>
2022-02-11 17:34:37 -0500

    worked through fcc tests

--- src/app.js
+++ src/app.js
@@ -1,15 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useId } from 'react';
 import ReactDOM from 'react-dom';
 import ReactMarkdown from 'react-markdown';
 
 function App() {
-	const [text, setText] = useState('');
+	let initialText = `
+# Welcome to my React Markdown Previewer!
 
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`;
+	// this is multi-line code:
+
+	function anotherExample(firstLine, lastLine) {
+		if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+			return multiLineCode;
+		}
+	}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.org), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbered lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+
+
+`;
+
+	const [text, setText] = useState(initialText);
 	return (
 		<>
 			<h1>Editor</h1>
-			<textarea value={text} onChange={e => setText(e.target.value)} />
+			<textarea id="editor" value={text} onChange={e => setText(e.target.value)} />
 			<h1>Preview</h1>
+
 			<ReactMarkdown>{text}</ReactMarkdown>
 		</>
 	);
